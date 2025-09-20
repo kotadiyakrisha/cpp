5361469a1d123d5ba5f05af9b421131c035e4a7b
kotadiyakrisha <krishakotadiya0@gmail.com>
2025-09-20 11:45:52 +0530

    Add files via upload

---
+++ display.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	    cout << "Name   : krisha kotadiya\n";
+	    cout << "City   : junagadh\n";
+	    cout << "Adress :85-A,jogi park\n";
+	    cout << "Pincode:362001\n";
+	    
+	    return 0;
+}
